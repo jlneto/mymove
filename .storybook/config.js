730d83f2b1276c8eff58b9ad8ad24275ac937d82
John Gedeon <john@truss.works>
2020-02-27 01:39:23 +0000

    Add todo to note we should scan stories directory

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,7 @@ import 'loki/configure-react';
 import '../src/index.scss';
 
 function loadStories() {
+  // TODO automatically scan stories dir see react-uswds repo for example
   require('../src/stories/index.stories.jsx');
   require('../src/stories/statusTimeLine.stories.jsx');
   require('../src/stories/tabNav.stories.jsx');
